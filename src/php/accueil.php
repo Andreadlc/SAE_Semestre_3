a82d0c835c2fc81b534b561e912a3dbd00b26e3b
Andreadlc <andreadelcros7@gmail.com>
2024-12-28 22:05:50 +0100

    ajout d'un message de confirmation d'une connexion a la page d'accueil

--- src/php/accueil.php
+++ src/php/accueil.php
@@ -8,7 +8,14 @@
 </head>
 <body>
 <?php include("includes/nav_bar.php"); ?>
-
+<div class="main-container">
+    <?php
+    if (isset($_SESSION['success_message_connexion'])) {
+        echo "<p class='success-message'>" . $_SESSION['success_message_connexion'] . "</p>";
+        unset($_SESSION['success_message_connexion']);
+    }
+    ?>
+</div>
 <?php include("includes/main.html"); ?>
 
 <?php include("includes/footer.html"); ?>
