c59fdfaefcb9cf78606ffd1114ceb0f3dde04030
Andreadlc <andreadelcros7@gmail.com>
2024-12-28 22:07:30 +0100

    ajout d'un message d'enregistrement si un admin se deconnecte

--- src/php/AdminLg.php
+++ src/php/AdminLg.php
@@ -3,6 +3,20 @@
 
 
 session_start();
+
+
+
+// Vérifier si un utilisateur est connecté
+if (isset($_SESSION['username'])) {
+    // Récupérer le nom d'utilisateur avant de détruire la session
+    $uname = $_SESSION['username'];
+
+    // Message de log pour indiquer que l'utilisateur se déconnecte
+    $log_message = "L'Admin: $uname s'est déconnecté à " . date('Y-m-d H:i:s') . "\n";
+    file_put_contents("logs/suppressions.log", $log_message, FILE_APPEND);
+}
+
+
 // Terminer la session
 session_destroy(); // Détruit la session et ses données
 
