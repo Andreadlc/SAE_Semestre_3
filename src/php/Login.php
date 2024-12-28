db77e510b46f157679cba49fec56a4d959fbfce6
Andreadlc <andreadelcros7@gmail.com>
2024-12-28 22:33:34 +0100

    ajout du fonctionnement du captcha pour la connexion et ajout de message pour le module de math

--- src/php/Login.php
+++ src/php/Login.php
@@ -1,28 +1,47 @@
 <?php
-
-
-
 include("includes/header.html");
 include("includes/nav_bar.php");
+?>
 
+    <div class="login-container">
+        <?php
+        // Vérifiez si un message d'erreur ou de succès est défini
+        if (isset($_SESSION['error_message'])) {
+            echo "<p style='color: #b22222; font-weight: bold;'>" . $_SESSION['error_message'] . "</p>";
+            unset($_SESSION['error_message']); // Supprimez le message après l'avoir affiché
+        } elseif (isset($_SESSION['success_message_connexion'])) {
+            echo "<p style='color: #006400; font-weight: bold;'>" . $_SESSION['success_message_connexion'] . "</p>";
+            unset($_SESSION['success_message_connexion']); // Supprimez le message après l'avoir affiché
+        }
+        ?>
 
-if (isset($_SESSION['success_message_connexion'])) {
-    echo "<p style='color: green;'>" . $_SESSION['success_message_connexion'] . "</p>";
-    unset($_SESSION['success_message_connexion']); // Supprimer le message après affichage
-}
+        <br><br><br>
 
+        <fieldset>
+            <legend>Authentification</legend>
+            <form method='post' action='Connexion.php'>
+                <label for='username'>Nom d'utilisateur</label>
+                <input type='text' name='username' id='username' placeholder='Nom utilisateur' required>
 
-echo "<br><br><br>";
+                <label for='password'>Mot de passe</label>
+                <input type='password' name='password' id='password' placeholder='Mot de passe' required>
 
-echo "<fieldset><legend>Authentification</legend>
-<form method='post' action='Connexion.php'>
-<label for='username'>Nom d'utilisateur</label> 
-<input type='text' name='username' id='username' placeholder='Nom utilisateur'> <!-- Added id -->
-<label for='password'>Mot de passe</label> 
-<input type='password' name='password' id='password' placeholder='Mot de passe'> <!-- Added id -->
-<input type='submit' name='ok' value='Se connecter'>
-<br>
-</form></fieldset>";
+                <table>
+                    <tr>
+                        <td>
+                            <label for="captcha">Veuillez entrer le code affiché</label>
+                            <input name='captcha' type='text' id="captcha" required>
+                            <img src='captcha.php' style='vertical-align: middle;' alt="captcha"/>
+                        </td>
+                    </tr>
+                </table>
 
+                <input type='submit' name='ok' value='Se connecter'>
+                <br>
+            </form>
+        </fieldset>
+    </div>
 
+<?php
 include("includes/footer.html");
+?>
--- src/php/module.php
+++ src/php/module.php
@@ -6,7 +6,25 @@ include("includes/header.html");
 include("includes/nav_bar.php");
 include("form.php");
 
+if (!isset($_SESSION['user_logged_in']) || $_SESSION['user_logged_in'] !== true) {
+    header('Location: acces_refuser.php');
+    exit();
+}
+?>
+<div class="registration-container">
+    <?php
+    if (isset($_SESSION['success_message'])) {
+        echo "<p style='color: #006400;'>" . $_SESSION['success_message'] . "</p>";
+        unset($_SESSION['success_message']); // Supprime le message après l'affichage
+    }
 
+    if (isset($_SESSION['error_message'])) {
+        echo "<p style='color: #b22222;'>" . $_SESSION['error_message'] . "</p>";
+        unset($_SESSION['error_message']); // Supprime le message après l'affichage
+    }
+    ?>
+</div>
+<?php
 // Traitement des données si le formulaire est soumis
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     include("form_processus.php");
