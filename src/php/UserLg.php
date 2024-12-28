254293964abe80a37ebce699d7d51b719d0bfc2e
Andreadlc <andreadelcros7@gmail.com>
2024-12-28 22:43:17 +0100

    ajout du message lorsqu'un user se deconnecte et creation du dossier logs et du fichier log

--- src/php/UserLg.php
+++ src/php/UserLg.php
@@ -4,6 +4,22 @@
 
 session_start();
 // Terminer la session
+
+
+
+// Vérifier si un utilisateur est connecté
+if (isset($_SESSION['username'])) {
+    // Récupérer le nom d'utilisateur avant de détruire la session
+    $uname = $_SESSION['username'];
+
+    // Message de log pour indiquer que l'utilisateur se déconnecte
+    $log_message = "L'utilisateur: $uname s'est déconnecté à " . date('Y-m-d H:i:s') . "\n";
+    file_put_contents("logs/suppressions.log", $log_message, FILE_APPEND);
+}
+
+// Terminer la session
 session_destroy(); // Détruit la session et ses données
 
+// Redirection vers la page d'accueil
 header("Location: accueil.php");
+exit();
