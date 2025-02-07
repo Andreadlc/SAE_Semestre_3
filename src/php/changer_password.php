d6e66384065e8a57437a45b208d33097f9164e1d
Andreadlc <andreadelcros7@gmail.com>
2025-02-07 20:06:30 +0100

    oublie de nom de variable dans changer_password

--- src/php/changer_password.php
+++ src/php/changer_password.php
@@ -47,7 +47,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         // Mise à jour du mot de passe
         $update_sql = "UPDATE utilisateur SET mot_de_passe = ? WHERE id = ?";
         $update_stmt = mysqli_prepare($co, $update_sql);
-        mysqli_stmt_bind_param($update_stmt, 'si', $new_password_md5, $user_id);
+        mysqli_stmt_bind_param($update_stmt, 'si', $new_password_encrypted , $user_id);
         mysqli_stmt_execute($update_stmt);
 
         if (mysqli_stmt_affected_rows($update_stmt) > 0) {
